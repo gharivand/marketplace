2e8f6cb4ef6358718e815524ccd38e4a3f22844c
mohsen <mohsen.gharivand@gmail.com>
2023-08-17 11:14:00 +0330

    implement loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,34 @@
+import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
+import React from 'react';
+import {Colors} from '../theme/colors';
+
+interface IProps {
+  isLoading: boolean;
+}
+
+const Loading: React.FC<IProps> = props => {
+  if (!props.isLoading) return null;
+  return (
+    <View style={style.container}>
+      <ActivityIndicator color={Colors.black} size={'small'} />
+      <Text style={style.title}>{'Fetching...'}</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 16,
+    marginVertical: 64,
+  },
+  title: {
+    fontWeight: '700',
+    color: Colors.black,
+    fontSize: 28,
+    textAlign: 'center',
+  },
+});
+
+export default Loading;
